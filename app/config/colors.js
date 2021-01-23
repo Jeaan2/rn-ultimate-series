bf3d222e0e7e32598324d3548d59cbfefbb70bd3
Jean Carlo da Silva <jeaan3@gmail.com>
2021-01-23 14:28:52 -0300

    Styling section

--- app/config/colors.js
+++ app/config/colors.js
@@ -2,5 +2,7 @@ export default {
     primary: "#fc4c65",
     secondary: "#4ecdc4",
     black: "#000",
+    white: '#fff',
+    medium: '#6e6969',
     
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+        blurRadius={10}
         style={styles.background}
         source={require('../assets/background.jpg')}>
 
             <View style={styles.logoContainer} >
             <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-            <Text> Sell what you don't need</Text>
+            <Text style={styles.tagline}> Sell what you don't need</Text>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" />
+                <AppButton title="Register" color="secondary" />
+            </View>
+
         </ImageBackground>
     );
 }
@@ -24,15 +29,9 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: 'center',
     },
-    loginButton: { 
-        width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65",
-    },
-    registerButton: { 
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4",
+    buttonsContainer: { 
+        padding: 20,
+        width: '100%',
     },
     logo: {
         width: 100,
@@ -42,6 +41,11 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 70,
         alignItems: 'center',
+    },
+    tagline: { 
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20,
     }
 
 
