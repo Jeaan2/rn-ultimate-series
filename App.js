6630e4a892ba52e76cb5e5b1fa84a1fe7d88bf69
Jean Carlo da Silva <jeaan3@gmail.com>
2021-01-25 21:35:01 -0300

    Input Components Section

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Switch, Text, TextInput, View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
@@ -14,12 +14,25 @@ import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
 
+const categories = [
+   {label: "Furniture", value: 1},
+   {label: "Clothing", value: 2},
+   {label: "Cameras", value: 3},
+]
 
 export default function App() {
+const [category, setCategory] = useState(categories[0]);
+ const [firstName, setFirstName] = useState('');
+ const [isNew, setIsNew] = useState(false);
 
   return (
-  <ListingsScreen />
+    <Screen>
+      <AppPicker selectedItem={category} onSelectItem={item => setCategory(item)} items={categories} icon="apps"placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email"/>
+    </Screen>
   );
 }
 
