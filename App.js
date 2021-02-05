7bb5a961dbd45f30eb6450cba1a12ac2df4ed150
Jean Carlo da Silva <jeaan3@gmail.com>
2021-02-04 21:38:36 -0300

    Part 2 - Navigation (Stacknavigation)

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 
-import React, {useState, useEffect} from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
+import React from 'react';
 import Screen from './app/components/Screen';
-import { Button, Image } from 'react-native';
+import {  Text } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-import ImageInput from './app/components/ImageInput';
-import ImageInputList from './app/components/ImageInputList';
-import ListingEditScreen from './app/screens/ListingEditScreen';
 
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+)
+
+const TweetDetails = () => (
+  <Screen>
+  <Text>Tweet Details</Text>
+</Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+)
 export default function App() {
   
   return (
-    <ListingEditScreen />
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
