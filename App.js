7a3c91d66c92e3bb3d850c565a522f76cf919b6f
Jean Carlo da Silva <jeaan3@gmail.com>
2021-02-23 00:12:40 -0300

    Offline Support Section

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
+
 
 export default function App() {
   return (
+    <>
+    <OfflineNotice />
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
     </NavigationContainer>
+    </>
   );
 }
