bdbb8585e10872b8df38fb5b102a1a1162d45c78
Jean Carlo da Silva <jeaan3@gmail.com>
2021-02-28 04:21:02 -0300

    Distribution section

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppLoading from 'expo-app-loading';
@@ -9,9 +11,13 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import { navigationRef } from "./app/navigation/rootNavigation";
+import logger from './app/utility/logger';
+
+logger.start();
 
 
 export default function App() {
+  logger.log(new Error("Error in app"));
   const [user, setUser] = useState()
   const [isReady, setIsReady] = useState(false)
 
